00b0ffaa6d5f5b51bc67abcc1e68710a41bd9047
iqbalchoironi <iqbal.choironi@gmail.com>
2020-07-15 08:51:29 +0700

    add search product per page api

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -5,15 +5,11 @@ module.exports = {
     addProductCategory: async (req, res) => {
         
         const { name } = req.body;
-        const createProductCategoryQuery = `INSERT INTO product_category(name) 
-                                    VALUES($1)
-                                    returning *`;
-        const values = [
-            name
-        ];
-
         try {
-            const { rows } = await query(createProductCategoryQuery, values);
+            const { rows } = await query(
+                `INSERT INTO product_category(name) VALUES($1) returning *`, 
+                [name]
+            );
             const dbResponse = rows[0];
             console.log(dbResponse)
             delete dbResponse.create_at;
@@ -26,22 +22,19 @@ module.exports = {
             console.log(error);
         }
     },
+
     addProduct: async (req, res) => {
 
         const { name, description, stock, price, product_category_id } = req.body;
-        const createProductQuery = `INSERT INTO product(name,description,stock,price,product_category_id) 
-                                    VALUES($1, $2, $3, $4, $5)
-                                    returning *`;
-        const values = [
-            name,
-            description,
-            stock,
-            price,
-            product_category_id
-        ];
 
         try {
-            const { rows } = await query(createProductQuery, values);
+            const { rows } = await query(
+                `INSERT INTO product(name,description,stock,price,product_category_id) 
+                 VALUES($1, $2, $3, $4, $5)
+                 returning *`, 
+                [name,description,stock,price,product_category_id]
+            )
+
             const dbResponse = rows[0];
 
             delete dbResponse.create_at;
@@ -79,6 +72,23 @@ module.exports = {
                 [productId]
             )
             res.send(rows[0]);
+        } catch(error){
+
+        }
+    },
+
+    searchProduct: async (req, res) => {
+        let productPerPage = 10;
+        let page = req.params.page || 1;
+        let key = req.params.key;
+
+        let startFrom = page * productPerPage - productPerPage; 
+        try {
+            const { rows } = await query(
+                `SELECT * FROM product WHERE name LIKE $1 LIMIT $2 OFFSET $3`,
+                [`%${key}%`,productPerPage, startFrom]
+            )
+            res.send(rows);
         } catch(error){
 
         }
--- src/routes/productRoute.js
+++ src/routes/productRoute.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const router = express.Router();
-const { addProduct, addProductCategory, getProductPerPage, getProductById } = require('../controllers/productController');
+const { suggestionSearchProduct,searchProduct,addProduct, addProductCategory, getProductPerPage, getProductById } = require('../controllers/productController');
 const { verifyToken ,isAdmin } = require('../middlewares/verifyAuth');
 
 
-router.post('/product-category', verifyToken, isAdmin, addProductCategory);
 
 router.post('/product', verifyToken, isAdmin, addProduct);
+router.post('/product/category', verifyToken, isAdmin, addProductCategory);
 router.get('/product/page/:page', getProductPerPage);
+router.get('/product/search/:key/page/:page', searchProduct);
 router.get('/product/:id', getProductById);
+
 
 module.exports = router;
