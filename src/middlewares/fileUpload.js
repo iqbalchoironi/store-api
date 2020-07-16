de7329094d588af45f46e936ce2469285c0e0021
iqbalchoironi <iqbal.choironi@gmail.com>
2020-07-16 14:04:27 +0700

    add verify file in upload middleware

--- src/middlewares/fileUpload.js
+++ src/middlewares/fileUpload.js
@@ -1,15 +1,30 @@
 const multer  = require('multer');
+const path = require('path');
+
+const uploadFolder = __dirname + '../../../uploads/images';
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
         cb(null, uploadFolder)
     },
     filename: function (req, file, cb) {
-      //const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
-      cb(null, Date.now() + path.extname(file.originalname))
+        let datetimestamp = Date.now();
+        cb(null, file.fieldname + '-' + datetimestamp + '.' + file.originalname.split('.')[file.originalname.split('.').length -1])
     }
 })
 
-const upload = multer({ storage: storage });
+const upload = multer({ 
+    storage: storage,
+    fileFilter: function (req, file, callback) {
+        var ext = path.extname(file.originalname);
+        if(ext !== '.png' && ext !== '.jpg' && ext !== '.gif' && ext !== '.jpeg') {
+            return callback(new Error('Only images are allowed'))
+        }
+        callback(null, true)
+    },
+    limits:{
+        fileSize: 1024 * 1024
+    } 
+});
 
 module.exports = { upload }
